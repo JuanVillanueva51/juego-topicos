8182693e729b4881329854245e2c8c3d31114811
juan.villanueva51 <juan.villanueva51@uabc.edu.mx>
2026-04-22 23:11:38 -0700

    Movimiento de enemigos y proyectiles

--- scripts/Bee.cs
+++ scripts/Bee.cs
@@ -1,16 +1,68 @@
 using Godot;
 using System;
 
-public partial class Bee : Node2D
+public partial class Bee : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export] public PackedScene proyectilScene;
+	private Node2D player;
+	private AnimatedSprite2D sprite;
+	private float speed = 100f;
+	private float shootTimer = 0f;
+	private float shootCooldown = 2f;
+	
 	public override void _Ready()
 	{
+		player = GetNode<Node2D>("/root/Game/player");
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		GD.Print(player);
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		if (player == null) return;
+		shootTimer -= (float)delta;
+		if (shootTimer <= 0f)
+		{
+		Shoot();
+		shootTimer = shootCooldown;
+		}
+		Vector2 direccion = (player.GlobalPosition - GlobalPosition).Normalized();
+		Velocity = direccion * speed;
+		MoveAndSlide();
+		UpdateAnimation(direccion);
+	}
+	void Shoot()
+	{
+		if(proyectilScene == null) return;
+		var proyectil = proyectilScene.Instantiate<Node2D>();
+		proyectil.GlobalPosition = GlobalPosition;
+		GetTree().CurrentScene.AddChild(proyectil);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	void UpdateAnimation(Vector2 direccion)
 	{
-		
+		if (direccion.Y > 0.2)
+		{
+			PlayAnim("walk_DOWN");
+		}
+		else if (direccion.Y < -0.2)
+		{
+			PlayAnim("walk_UP");
+		}
+		else if (direccion.X > 0.2)
+		{
+			sprite.FlipH = true;
+			PlayAnim("walk_LEFT");
+		}
+		else if (direccion.X < -0.2)
+		{
+			sprite.FlipH = false;
+			PlayAnim("walk_LEFT");
+		}
+	}
+
+	void PlayAnim(string anim)
+	{
+		if (sprite.Animation != anim)
+			sprite.Play(anim);
 	}
 }
