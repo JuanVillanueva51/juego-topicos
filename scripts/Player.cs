a8365dfa1b527da306c5d74cdce44a7d369954ef
DanielEspinoza20 <daniel.espinoza.cano@uabc.edu.mx>
2026-04-25 22:15:05 -0700

    Controles laterales

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,21 +4,19 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	
+	private AnimatedSprite2D sprite;
+
 	public override void _Ready()
 	{
 		AddToGroup("player");
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 	}
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 		// Obtener dirección en X y Y
-<<<<<<< HEAD
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-=======
-		Vector2 direction = Input.GetVector("Move_LEFT", "Move_RIGHT", "Move_UP", "Move_DOWN");
->>>>>>> 850d756a76585f7cea086314613e4caa21ab6be4
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
@@ -32,5 +30,50 @@ public partial class Player : CharacterBody2D
 		}
 		Velocity = velocity;
 		MoveAndSlide();
+		
+		UpdateAnimation();
+	}
+	private void UpdateAnimation()
+	{
+		if (Velocity.Y > 0.2f)
+		{
+			PlayAnim("walk_Down");
+		}
+		else if (Velocity.Y < -0.2f)
+		{
+			PlayAnim("walk_Up");
+		}
+		else if (Velocity.X > 0.2f)
+		{
+			sprite.FlipH = true;
+			PlayAnim("walk_Left_Down");
+		}
+		else if (Velocity.X < -0.2f)
+		{
+			sprite.FlipH = true;
+			PlayAnim("walk_Right_Down");
+		}
+		if (Velocity.X > 0.2f && Velocity.Y > 0.2f){
+			sprite.FlipH = false;
+			PlayAnim("walk_Right_Down");
+		}
+		else if (Velocity.X < -0.2f && Velocity.Y > 0.2f){
+			sprite.FlipH = false;
+			PlayAnim("walk_Left_Down");
+		}
+		else if (Velocity.X > 0.2f && Velocity.Y <  -0.2f){
+			sprite.FlipH = false;
+			PlayAnim("walk_Right_Up");
+		}
+		else if (Velocity.X < -0.2f && Velocity.Y < -0.2f){
+			sprite.FlipH = false;
+			PlayAnim("walk_Left_Up");
+		}
+	}
+	
+	private void PlayAnim(string anim)
+	{
+		if (sprite.Animation != anim)
+			sprite.Play(anim);
 	}
 }
